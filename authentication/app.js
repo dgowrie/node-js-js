b029d33ccf23189dfbee2802c26f5c9a79347ae9
David Gowrie <david.gowrie@gmail.com>
2014-11-07 17:16:48 -0500

    setting up routes, adding route for 'form' test

--- authentication/app.js
+++ authentication/app.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+    routes = require('./routes/index'),
+    users = require('./routes/users'),
 
-var app = express();
+    app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', routes);
 app.use('/users', users);
 
--- authentication/routes/index.js
+++ authentication/routes/index.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+	router = express.Router(),
+
+    // temp file system reading
+    fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+
+/*
+ * Routes
+ */
+ // Best practice would be setting up separate route files for different parts
+ // of your app. For example, the users route file might contain routes for 
+ // adding users, deleting them, updating them, and so forth, while a new 
+ // route file called "locations" might handle adding, editing, deleting 
+ // and displaying location data (in an app for which that was required). 
+ // In this case, to keep things simple, we're going to do everything in 
+ // the index router.
+
+// Form.html route
+router.get('/form', function(req, res) {
+    fs.readFile('./public/form.html', function(error, content) {
+        if (error) {
+            res.writeHead(500);
+            res.end();
+        }
+        else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf-8');
+        }
+    });
 });
 
 module.exports = router;
